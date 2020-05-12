e0aefde0b6eebedce9d48dc37ec55524b5e3be0f
evitch <yuuta040208@gmail.com>
2020-05-12 21:11:12 +0900

    ボーダー設定機能を実装

--- app/controllers/api/v1/odds_controller.rb
+++ app/controllers/api/v1/odds_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::OddsController < Api::V1::ApplicationController
         body = JSON.parse(request.body.read)
         if body['odds'].values.sum <= 1.0
 
-          render json: bets(race, body['odds'], body['budget']).to_json
+          render json: bets(race, body['odds'], body['budget'], body['border']).to_json
         else
           render json: {error: true, message: 'データ形式が無効です。'}
         end
@@ -24,7 +24,7 @@ class Api::V1::OddsController < Api::V1::ApplicationController
 
   private
 
-  def bets(race, body, budget)
+  def bets(race, body, budget, border = nil)
     odds = race.odds
 
     bets = {}
@@ -35,26 +35,30 @@ class Api::V1::OddsController < Api::V1::ApplicationController
       sum = except_bet_body.sum {|_, b| b}
       bet_counts = except_bet_body.map {|a, b| [a, (b.to_f / sum * bet_max).ceil(0)]}.select {|_, b| !b.zero?}
 
-      temp_count = 0
-      bet_counts = bet_counts.select do |_, b|
-        temp_count = temp_count + b
-        temp_count <= bet_max
-      end
-
+      t_count = 0
       bet_counts.each do |a, b|
-        numbers = [horse_number, a].sort.join('-')
+        b.times do
+          break if t_count >= bet_max
 
-        if bets[numbers].present?
-          bet = bets[numbers]
-          bets[numbers] = {
-              count: b + bet[:count],
-              value: (b * odds.find_by(first_horse_number: horse_number, second_horse_number: a).quinella * 100).round(0) + bet[:value]
-          }
-        else
-          bets[numbers] = {
-              count: b,
-              value: (b * odds.find_by(first_horse_number: horse_number, second_horse_number: a).quinella * 100).round(0)
-          }
+          numbers = [horse_number, a].sort.join('-')
+          quinella = odds.find_by(first_horse_number: horse_number, second_horse_number: a).quinella
+
+          if bets[numbers].present?
+            bet = bets[numbers]
+            break if border.present? && bet[:value] > budget * 100 * border
+
+            bets[numbers] = {
+                count: bet[:count] + 1,
+                value: (quinella * 100).round(0) + bet[:value]
+            }
+          else
+            bets[numbers] = {
+                count: 1,
+                value: (quinella * 100).round(0)
+            }
+          end
+
+          t_count = t_count + 1
         end
       end
     end
